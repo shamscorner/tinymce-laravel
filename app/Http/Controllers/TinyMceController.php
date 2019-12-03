13d23832e9608d23bcfa409d74c1afdea6f9004c
Shamim Hossain <hossains159@gmail.com>
2019-12-03 21:56:23 +0600

    Initial Commit

---
+++ app/Http/Controllers/TinyMceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TinyMceController extends Controller
+{
+    /**
+    * Author: shamscorner
+    * DateTime: 03/December/2019 - 21:44:20
+    *
+    * main entry point
+    *
+    */
+    public function index()
+    {
+        return view('welcome');
+    }
+}
